9c9034699e0e5fc47c857ea22d23fa9916923a32
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2022-11-14 21:20:14 +0300

    refactoring styles

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -5,7 +5,7 @@ export const Popup = ({ text, closePopup }) => {
     return (
         <div className="popup-container">
             <div className="popup-body">
-                <h1 className="successText">Заказ для номера {text} принят</h1>
+                <h2 className="successText">Заказ для номера {text} принят</h2>
                 <button className="btn" onClick={closePopup}>Закрыть</button>
             </div>
         </div>
